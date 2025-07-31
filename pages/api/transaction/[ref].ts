1b89e3bd12f83ebb0df2f78d73b15e4a787270ec
cedoe70 <cedoe70@gmail.com>
2025-07-31 04:42:45 -0500

    Update [ref].ts

--- pages/api/transaction/[ref].ts
+++ pages/api/transaction/[ref].ts
@@ -1,6 +1,6 @@
 // pages/api/transaction/[ref].ts
 import type { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "@/lib/prisma";
+import { prisma } from "../../../lib/prisma";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { ref } = req.query;
