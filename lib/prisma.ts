5a1a8f46a75a495eac11950b8b41dc64edbbeb45
cedoe70 <cedoe70@gmail.com>
2025-07-31 04:35:26 -0500

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,7 +8,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
+    log: ['query'], // optional: helpful during development
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
